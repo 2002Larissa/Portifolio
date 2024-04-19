6bea61b5190013a3a535362fe5d917544015edcb
2002Larissa <larissadssantos1515@gmail.com>
2024-04-19 08:58:25 -0300

    Atualização de home

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-var typed = new typed(".text", {
+var typed= new typed(".text", {
     Strings:["Desenvolvedora Frontend", "youtuber", "designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop: true
+    loop:true
 })
